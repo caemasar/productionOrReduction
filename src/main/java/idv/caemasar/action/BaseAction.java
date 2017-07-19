ec773e32c34c08d34140e2c2dc1414312fa9fa64
Caemasar <caemasar@protonmail.com>
2017-07-19 11:30:56 +0800

    框架配置通过。

---
+++ src/main/java/idv/caemasar/action/BaseAction.java
@@ -0,0 +1,12 @@
+package idv.caemasar.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 588465610991252986L;
+
+}
---
+++ src/main/java/idv/caemasar/action/IndexAction.java
@@ -0,0 +1,17 @@
+package idv.caemasar.action;
+
+public class IndexAction extends BaseAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -32898234941940108L;
+	private static final String PATH_INDEX = "index";
+
+	// 编写execute()方法
+	public String execute() {
+
+		return PATH_INDEX;
+	}
+
+}
